20b9279a70e23b53387f9c29eb17d59abddec641
rsperlich <lordgador99@gmail.com>
2016-06-16 17:14:52 +0200

    added search and sort

--- 2016-06-13_Workout-Manager_ASP.NET-MVC/Controllers/ExerciseController.cs
+++ 2016-06-13_Workout-Manager_ASP.NET-MVC/Controllers/ExerciseController.cs
@@ -14,22 +14,20 @@ namespace _2016_06_13_Workout_Manager_ASP.NET_MVC.Controllers
     {
         private TechnikmarktEntities db = new TechnikmarktEntities();
 
-        public ActionResult Index(string sortOrder)
+        public ActionResult Index(string sortOrder, string searchString)
         {
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
             var exercises = from e in db.exercises
                             select e;
-            switch (sortOrder)
+
+            if (!String.IsNullOrEmpty(searchString)) {
+                exercises = exercises.Where(s => s.e_name.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+                switch (sortOrder)
             {
                 case "name_desc":
                     exercises = exercises.OrderByDescending(e => e.e_name);
-                    break;
-                case "Date":
-                    exercises = exercises.OrderBy(e => e.e_id);
-                    break;
-                case "date_desc":
-                    exercises = exercises.OrderByDescending(e => e.e_id);
                     break;
                 default:
                     exercises = exercises.OrderBy(e => e.e_name);
